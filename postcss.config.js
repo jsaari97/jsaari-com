493baa6660077059c3f44bb43049d16f11c88364
Jim Saari <jim.saari97@gmail.com>
2020-04-22 21:27:51 +0300

    tweak postcss settings

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
-    require("postcss-preset-env")({ stage: 0 }),
+    require("postcss-preset-env")({
+      stage: 3,
+      features: {
+        "nesting-rules": true,
+      },
+    }),
     ...(process.env.HUGO_ENVIRONMENT === "production"
       ? [require("cssnano")]
       : []),
