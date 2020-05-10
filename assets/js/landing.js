6c331da0991132e768ce7072edb2503372b83ca9
Jim Saari <jim.saari97@gmail.com>
2020-05-10 22:45:52 +0300

    run hero animation after assets load

---
+++ assets/js/landing.js
@@ -0,0 +1,15 @@
+const heroImageLoader = () => {
+  const img = document.querySelector('.hero--profile > img')
+
+  const handleLoad = () => {
+    img.parentElement.classList.add('loaded');
+  }
+
+  if (img.complete) {
+    handleLoad()
+  } else {
+    img.addEventListener('load', handleLoad)
+  }
+}
+
+heroImageLoader()
